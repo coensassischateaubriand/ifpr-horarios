80f9266800e558363ffa4fcc058116d25af5f0f1
vagnersantosbsi <vagnersantos.bsi@gmail.com>
2025-10-26 18:11:19 -0300

    Ajuste no link de produção.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -57,7 +57,7 @@ const config: Config = {
    favicon: 'img/favicon.gif',
 
   // Set the production url of your site here
-  url: 'https://coensassischateaubriand.github.io/',
+  url: 'https://horario-ifpr-assis-chateaubriand.netlify.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   
